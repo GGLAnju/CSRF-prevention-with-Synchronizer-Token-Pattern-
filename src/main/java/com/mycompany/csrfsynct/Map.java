2b952783cb3304a6293d27b79d4a01824ea90ace
Lasith Anjula <41545476+GGLAnju@users.noreply.github.com>
2018-10-09 11:52:27 +0530

    Update Map.java

--- src/main/java/com/mycompany/csrfsynct/Map.java
+++ src/main/java/com/mycompany/csrfsynct/Map.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.csrfsynct;
 
 import java.util.HashMap;
 
-/**
- *
- * @author KALINDU
- */
 public class Map {
 	public static HashMap<String, String> hashMap = new HashMap<String, String>();
 	
